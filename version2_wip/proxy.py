38d8548703d2a4830e3bc3be46e5f1ccd002538d
cemkata <cemkata@users.noreply.github.com>
2026-04-11 17:59:59 +0300

    Tested and working version 2

--- version2_wip/proxy.py
+++ version2_wip/proxy.py
@@ -15,9 +15,9 @@ auch_port =     data['authentication_server']['port']
 auth_username = data['authentication_server']['auth_username']
 auth_password = data['authentication_server']['auth_password']
 
-dest_protocol = data['destination_server']['protocol']
-dest_server =   data['destination_server']['ip']
-dest_port =     data['destination_server']['port']
+dest_protocol = data['proxy_server']['destination_server']['protocol']
+dest_server =   data['proxy_server']['destination_server']['ip']
+dest_port =     data['proxy_server']['destination_server']['port']
 
 # ===== Helpers =====
 def cors(resp):
@@ -35,23 +35,24 @@ def check_login(request):
         else:
             targer_url = f'{auch_protocol}://{auch_server}:{auch_port}'
     else:
-        if request.path.decode("utf-8") == "/logout" or request.path.decode("utf-8") == "/logoff": 
+        if request.path == "/logout" or request.path == "/logoff": 
             targer_url = f'{auch_protocol}://{auch_server}:{auch_port}' + "/logout"
         else:
-            if request.path.decode("utf-8") == "/login":
-                #targer_url = f'{dest_protocol}://{auch_server}:{dest_port}'
-                targer_url = f'{dest_protocol}://{dest_server}:{dest_port}'
+            if request.path == "/login":
+                targer_url = f'{dest_protocol}://{auch_server}:{dest_port}'
+                #targer_url = f'{dest_protocol}://{dest_server}:{dest_port}'
             else:
                 authenticated = True
-                targer_url = f'{dest_protocol}://{dest_server}:{dest_port}' + request.path.decode("utf-8")
+                targer_url = f'{dest_protocol}://{dest_server}:{dest_port}' + request.full_path
     return (targer_url, authenticated)
 
-def proccess_response(req, authenticated):
+def proccess_response(req, authenticated, origin_server):
+    return_data = req.content
     if req.status_code != 200:
         new_headers = {}
         for key in req.headers:
             new_headers.update({
-                key.encode("utf-8"): req.headers[key].encode("utf-8"),
+                key: req.headers[key],
             })
         try:
             if (dest_protocol == 'http' and dest_port == '80') or (dest_protocol == 'https' and dest_port == '443'):
@@ -71,11 +72,10 @@ def proccess_response(req, authenticated):
                         stringToreplace = stringToreplace + '>'
                         break
                     stringToreplace = stringToreplace + ch
-                req.content = req.content.replace(stringToreplace.encode('utf-8'), stringToreplace.encode('utf-8')+b'<div style="background-color:LightYellow;"><a href="/logout" style="color:blue;text-align:right; float: right;" >Logoff</a><br></div>')
+                return_data = return_data.replace(stringToreplace.encode('utf-8'), stringToreplace.encode('utf-8')+b'<div style="background-color:LightYellow;"><a href="/logout" style="color:blue;text-align:right; float: right;" >Logoff</a><br></div>')
             except IndexError:
                request_response = req.content
-        resp = Response(req.content, status=req.status_code, content_type=req.headers['Content-Type'])
-        #resp.headers = new_headers
+        resp = Response(return_data, status=req.status_code, content_type=req.headers['Content-Type'])
     return cors(resp)
 
 @app.route('/')
@@ -84,62 +84,64 @@ def process_index():
 
 @app.route('/<path:url_path>', methods = ['POST', 'GET', 'PUT', 'DELETE', 'PATCH'])
 def process_request(url_path):
-    url, authenticated = check_login(request)
-    if not authenticated:
-        if url_path.startswith('static/'):
-            url = f'{auch_protocol}://{auch_server}:{auch_port}/{url_path}'
-    origin_server = ""
-    server_request_headers = {}
-    for header in request.headers:
-        if header[0] == 'Host' or header[0] == 'Referer' or header[0] == 'Origin':
-            if header[0] == 'Origin':
-                origin_server = header[1]
-            continue
-        server_request_headers[header[0]] = header[1]
-    if request.method == 'POST':
-        req = requests.post(url,
-            headers=server_request_headers,
-            data = request.form,
-            auth=HTTPBasicAuth(auth_username, auth_password),
-            allow_redirects=False
-        )
-    elif request.method == 'PUT':
-        req = requests.put(
-            url, 
-            data = request.form, 
-            headers=server_request_headers,
-            auth=HTTPBasicAuth(auth_username, auth_password),
-            allow_redirects=False,
-        )
-    elif request.method == 'DELETE':
-        req = requests.delete(
-            url, 
-            data = request.form, 
-            headers=server_request_headers,
-            auth=HTTPBasicAuth(auth_username, auth_password),
-            allow_redirects=False,
-        )
-    elif request.method == 'PATCH':
-        req = requests.patch(
-            url, 
-            data = request.form, 
-            headers=server_request_headers,
-            auth=HTTPBasicAuth(auth_username, auth_password),
-            allow_redirects=False,
-        )
-    else:
-        try:
+    try:
+        url, authenticated = check_login(request)
+        if not authenticated:
+            if url_path.startswith('static/'):
+                url = f'{auch_protocol}://{auch_server}:{auch_port}/{url_path}'
+        origin_server = ""
+        server_request_headers = {}
+        for header in request.headers:
+            if header[0] == 'Host' or header[0] == 'Referer' or header[0] == 'Origin':
+                if header[0] == 'Origin':
+                    origin_server = header[1]
+                continue
+            server_request_headers[header[0]] = header[1]
+        if request.method == 'POST':
+            req = requests.post(url,
+                headers=server_request_headers,
+                data = request.form,
+                auth=HTTPBasicAuth(auth_username, auth_password),
+                allow_redirects=False
+            )
+        elif request.method == 'PUT':
+            req = requests.put(
+                url, 
+                data = request.form, 
+                headers=server_request_headers,
+                auth=HTTPBasicAuth(auth_username, auth_password),
+                allow_redirects=False,
+            )
+        elif request.method == 'DELETE':
+            print(">>>Delete")
+            req = requests.delete(
+                url, 
+                data = request.form, 
+                headers=server_request_headers,
+                auth=HTTPBasicAuth(auth_username, auth_password),
+                allow_redirects=False,
+            )
+        elif request.method == 'PATCH':
+            req = requests.patch(
+                url, 
+                data = request.form, 
+                headers=server_request_headers,
+                auth=HTTPBasicAuth(auth_username, auth_password),
+                allow_redirects=False,
+            )
+        else:
             req = requests.get(url,
                 headers=server_request_headers,
                 auth=HTTPBasicAuth(auth_username, auth_password),
                 allow_redirects=False,
             )
-        except Exception as e:
-            resp = Response(json.dumps({'error': str(e)}), status=502, content_type='application/json')
-            return cors(resp)
-    return proccess_response(req, authenticated)
-
-
+        return proccess_response(req, authenticated, origin_server)
+    except requests.exceptions.ConnectionError:
+        resp = Response(json.dumps({'error': "Server not accecible"}), status=500, content_type='application/json')
+        return cors(resp)
+    except Exception as e:
+        resp = Response(json.dumps({'error': str(e)}), status=502, content_type='application/json')
+        return cors(resp)
 if __name__ == '__main__':
     retries = 0
     max_retries = int(data['proxy_server']['startup_retries'])
